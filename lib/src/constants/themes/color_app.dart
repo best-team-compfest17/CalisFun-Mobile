a3554e6e389d1ff1ad15b3807a224ee4b8bc09f6
favjiw <mfavianj@gmail.com>
2025-08-13 13:40:29 +0700

    chore: add style based on design on figma

---
+++ lib/src/constants/themes/color_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ColorApp {
+  static const Color mainBlack = Color(0xFF121212);
+  static const Color mainWhite = Color(0xFFF7F8FA);
+  static const Color greyInactive = Color(0xFFD9D9D9);
+  static const Color primary = Color(0xFF3D7BF7);
+  static const Color secondary = Color(0xFFFFB703);
+  static const Color success = Color(0xFF06D6A0);
+  static const Color error = Color(0xFFE63946);
+  static const Color border = Color(0xFFD1D5DB);
+  static const Color easy = Color(0xFF06D6A0);
+  static const Color medium = Color(0xFFFFB703);
+  static const Color hard = Color(0xFFE63946);
+}
