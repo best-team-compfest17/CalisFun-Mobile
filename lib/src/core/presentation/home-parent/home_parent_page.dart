947f530f78c2ac87ce1127715a91d46b434474a1
favjiw <mfavianj@gmail.com>
2025-08-15 14:34:09 +0700

    feat:home parent slice not empty

--- lib/src/core/presentation/home-parent/home_parent_page.dart
+++ lib/src/core/presentation/home-parent/home_parent_page.dart
@@ -1,8 +1,11 @@
 import 'package:calisfun/src/widgets/app_button/app_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 
 import '../../../constants/constants.dart';
+import '../../../widgets/widgets.dart';
 
 class HomeParentPage extends ConsumerWidget {
   const HomeParentPage({super.key});
@@ -31,13 +34,50 @@ class HomeParentPage extends ConsumerWidget {
             Gap.h16,
             Text('Aktivitas Anak', style: TypographyApp.headingLargeBoldPrimary,),
             Gap.h16,
-            Image.asset('assets/images/parent_empty_img.png', width: SizeApp.customWidth(300), height: SizeApp.customHeight(200),),
-            Gap.h16,
-            Text(
-              'Belum ada akun anak? Daftarkan sekarang biar bisa mulai.',
-              style: TypographyApp.bodyNormalMedium,
-              textAlign: TextAlign.center,
+            AppContainer(
+              height: SizeApp.h80,
+              childLeft: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text('Zelin', style: TypographyApp.headingSmallBold,),
+                  Gap.w8,
+                  Text('Level 0', style: TypographyApp.headingSmallMedium,),
+                ],
+              ),
+              childRight: Container(
+                width: SizeApp.customWidth(46),
+                height: SizeApp.customHeight(46),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(100.r),
+                ),
+                //svg
+                child: Center(
+                  child: SvgPicture.asset(
+                    'assets/icons/arrow_next_ic.svg',
+                    width: SizeApp.w24,
+                    height: SizeApp.h24,
+                    colorFilter: const ColorFilter.mode(
+                      ColorApp.primary,
+                      BlendMode.srcIn,
+                    ),
+                  ),
+                ),
+              ),
+              showBorder: true,
+              showShadow: true,
+              backgroundColor: ColorApp.mainWhite,
             ),
+            // if-empty
+            // Text('Aktivitas Anak', style: TypographyApp.headingLargeBoldPrimary,),
+            // Gap.h16,
+            // Image.asset('assets/images/parent_empty_img.png', width: SizeApp.customWidth(300), height: SizeApp.customHeight(200),),
+            // Gap.h16,
+            // Text(
+            //   'Belum ada akun anak? Daftarkan sekarang biar bisa mulai.',
+            //   style: TypographyApp.bodyNormalMedium,
+            //   textAlign: TextAlign.center,
+            // ),
             Gap.h24,
             AppButton(text: 'Tambah', onPressed: (){})
           ],
--- lib/src/widgets/app_container/app_container.dart
+++ lib/src/widgets/app_container/app_container.dart
@@ -6,6 +6,11 @@ class AppContainer extends StatelessWidget {
   final Widget childLeft;
   final Widget childRight;
   final Color? backgroundColor;
+  final bool showBorder;
+  final bool showShadow;
+  final Color? borderColor;
+  final double borderWidth;
+  final double borderRadiusValue;
 
   const AppContainer({
     super.key,
@@ -13,17 +18,40 @@ class AppContainer extends StatelessWidget {
     required this.childLeft,
     required this.childRight,
     this.backgroundColor,
+    this.showBorder = false,
+    this.showShadow = false,
+    this.borderColor,
+    this.borderWidth = 1.0,
+    this.borderRadiusValue = 7.0,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: height ?? SizeApp.customHeight(120),
-      padding: EdgeInsets.symmetric(horizontal: SizeApp.h12, vertical: SizeApp.h16),
+      padding: EdgeInsets.symmetric(
+        horizontal: SizeApp.h12,
+        vertical: SizeApp.h16,
+      ),
       width: SizeApp.customWidth(360),
       decoration: BoxDecoration(
         color: backgroundColor ?? ColorApp.primary,
-        borderRadius: BorderRadius.circular(SizeApp.w16),
+        borderRadius: BorderRadius.circular(borderRadiusValue),
+        border: showBorder
+            ? Border.all(
+          color: borderColor ?? ColorApp.border,
+          width: borderWidth,
+        )
+            : null,
+        boxShadow: showShadow
+            ? [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 6,
+            offset: const Offset(0, 3),
+          ),
+        ]
+            : null,
       ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
